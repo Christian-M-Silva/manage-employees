41aa75e74168d2690404e1a78d4426588f9ea97c
Christian Silva <tininjacms@gmail.com>
2025-03-14 16:33:33 -0300

    feat(EmployePJ): Criará a class de empregado pj com seu calculo

---
+++ ManageEmployees/models/EmployerPj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageEmployees.models
+{
+    public class EmployerPj(string name, double baseSalary) : Emoloyees(name, baseSalary)
+    {
+        public int WorkHours { get; set; }
+
+        public override void CalculateSalary()
+        {
+            double finalSalary = WorkHours * BaseSalary;
+            Console.WriteLine($"{Name} trabalhou {WorkHours}:00. Sendo o seu sálario R${BaseSalary}, terá como sálario final {finalSalary}");
+        }
+    }
+}
