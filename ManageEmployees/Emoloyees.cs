3fb252c8589da20b0a82c2120efec8f98ceb13a5
Christian Silva <tininjacms@gmail.com>
2025-03-14 10:11:50 -0300

    feat(ClassEmoloyees): Add a class Emoloyees

---
+++ ManageEmployees/Emoloyees.cs
@@ -0,0 +1,18 @@
+﻿namespace ManageEmployees
+{
+    public abstract class Emoloyees
+    {
+        protected string? Name { get; set; }
+        protected double SalarioBase { get; set; }
+        public abstract void CalculateSalary();
+
+        delegate void emoloyeesRegisterHandler();
+        event emoloyeesRegisterHandler? OnEmoloyeesRegister;
+
+        public void Start()
+        {
+            CalculateSalary();
+            OnEmoloyeesRegister?.Invoke();
+        }
+    }
+}
