c4085aade0e36a644ceea1161e71f914c42ad4cf
Christian Silva <tininjacms@gmail.com>
2025-03-14 10:15:39 -0300

    feat(ClassBenefits): Add a class Benefits

---
+++ ManageEmployees/Benefits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageEmployees
+{
+    public class Benefits
+    {
+        public double MealVoucher { get; set; }
+        public double FoodVoucher { get; set; }
+        public double Total { get; set; }
+    }
+}
